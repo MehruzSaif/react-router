05012371062bef55920f0541037c011e9c54d6b5
MehruzSaif <mehruzsaif007@gmail.com>
2022-04-01 02:07:27 +0600

    added new component

---
+++ src/components/PostDetail/PostDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostDetail = () => {
+    return (
+        <div>
+            <h4>This is Post detail for: </h4>
+        </div>
+    );
+};
+
+export default PostDetail;
